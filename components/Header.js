21099ed3916aeaa02a89f05ead77e30cc9901e75
Manu Anish <manusanish@gmail.com>
2022-06-04 19:41:19 -0400

    done!

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
 import * as React from "react";
-import {RepoLockedIcon} from '@primer/octicons-react'
+import { RepoLockedIcon } from "@primer/octicons-react";
 
 function Header() {
   return (
     <div>
       <div className="fixed bg-white top-0 left-0 p-8 w-full z-50 border-b">
-        <div className="font-black"><RepoLockedIcon/> CMU PGP</div>
+        <div className="font-black">
+          <RepoLockedIcon /> CMU PGP
+        </div>
       </div>
       <div className="pb-[150px]"></div>
     </div>
